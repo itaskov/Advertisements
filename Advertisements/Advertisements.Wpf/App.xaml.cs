3a23569e0bd7d501e7b110967a9b67828d05091b
itaskov <itaskov@gmail.com>
2015-05-04 20:15:06 +0300

    Added Wpf app and tested service layer with it.

---
+++ Advertisements/Advertisements.Wpf/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Advertisements.Web.App_Start;
+
+namespace Advertisements.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var autoMapperConfig = new AutoMapperConfig();
+            autoMapperConfig.Execute();
+        }
+    }
+}
---
+++ Advertisements/Advertisements.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Advertisements.Infrastructures.Services;
+using Advertisements.Infrastructures.Services.Contracts;
+
+namespace Advertisements.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly IHomeServices homeServices;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.homeServices = new HomeServices();
+        }
+
+        public MainWindow(IHomeServices homeServices)
+        {
+            this.homeServices = homeServices;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var dataContext = this.homeServices.GetAllAds().ToList();
+            //this.GridAds.DataContext = dataContext;
+            this.DataGrid.ItemsSource = dataContext;
+        }
+    }
+}
